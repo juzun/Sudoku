5d16a11146439a208c9d90c443428ad791bd951d
DanInvest <danek.buj@gmail.com>
2021-04-21 20:03:35 +0200

    Pridany poznamky

--- X_algorithm.py
+++ X_algorithm.py
@@ -1,5 +1,5 @@
-import numpy as np
 from itertools import product
+import numpy as np
 
 def exact_cover(X, Y):
     X = {j: set() for j in X}
@@ -9,29 +9,28 @@ def exact_cover(X, Y):
     return X, Y
 
 def solve(X,Y,solution=[]):  
-    
-    if not X:  
+    if not X:   # je-li zadaný slovník prázdný, "máme" hned řešení
         yield list(solution) 
     else:
-        c = min(X, key=lambda c: len(X[c])) # vyber minimum ?? asi najdi sloupec s minimálním počtem jedniček
+        c = min(X, key=lambda c: len(X[c])) # najdi sloupec s minimálním počtem jedniček
         
-        for r in list(X[c]): 
-            solution.append(r)
-            cols = select(X, Y, r)
-            for s in solve(X, Y, solution):
+        for r in list(X[c]):  # iteruju přes všechna písmena ve slovníku X na řádku c
+            solution.append(r) # r-tý řádek přídaný do řešení 
+            cols = select(X, Y, r) # potřebuju najít jedničkové sloupce z tohoto řádku
+            for s in solve(X, Y, solution): # rekurze
                 yield s
-            deselect(X, Y, r, cols)
-            solution.pop()
+            # deselect(X, Y, r, cols)   # pro případ více řešení (smaže aktuální, hledá další) 
+            # solution.pop()
 
 def select(X, Y, r):
     
-    cols = []
-    for j in Y[r]:
-        for i in X[j]:
-            for k in Y[i]:
+    cols = [] # záloha vymazaných možností z X
+    for j in Y[r]: # j jsou čísla v řádku(písmenu) r  (1,4,7) 
+        for i in X[j]: # přes všechna písmena i v řádku (čísle) j (pro j=1 je to A,B)
+            for k in Y[i]: # opět 1,4,7
                 if k != j:
-                    X[k].remove(i)
-        cols.append(X.pop(j))
+                    X[k].remove(i) # odstranuju A,B,C,E,F 
+        cols.append(X.pop(j))  
     return cols
 
 
@@ -44,28 +43,37 @@ def deselect(X, Y, r, cols):
                     X[k].add(i)
 
 
+
 def solve_sudoku(size, grid):
     R, C = size
-    N = R * C
-    X = ([("rc", rc) for rc in product(range(N), range(N))] +
+    N = R * C  # počet pmožných čísel
+    X = ([("rc", rc) for rc in product(range(N), range(N))] +   # čísla n až od 1, sloupce a řádky od 0
          [("rn", rn) for rn in product(range(N), range(1, N + 1))] +
          [("cn", cn) for cn in product(range(N), range(1, N + 1))] +
          [("bn", bn) for bn in product(range(N), range(1, N + 1))])
+    # X prostě vektor 
+    
     Y = dict()
-    for r, c, n in product(range(N), range(N), range(1, N + 1)):
+    for r, c, n in product(range(N), range(N), range(1, N + 1)):  # všechny řádky v tabulce - také jen hlavičky R1C1#1 apod.
         b = (r // R) * R + (c // C) # Box number
         Y[(r, c, n)] = [
             ("rc", (r, c)),
             ("rn", (r, n)),
             ("cn", (c, n)),
             ("bn", (b, n))]
-    X, Y = exact_cover(X, Y)
+    
+    
+    X, Y = exact_cover(X, Y) # nX se předělá na dictionary
+   
     for i, row in enumerate(grid):
         for j, n in enumerate(row):
-            if n:
-                select(X, Y, (i, j, n))
-    for solution in solve(X, Y, []):
-        for (r, c, n) in solution:
+            if n: # if n>0 ??
+                select(X, Y, (i, j, n)) # pouze promazáváme možnosti v X 
+    
+
+    for solution in solve(X, Y, []):  # hlavní řešič ... solutions může být vice
+        
+        for (r, c, n) in solution:  # vypsání výsledného sudoku
             grid[r][c] = n
         yield grid
 
@@ -81,10 +89,15 @@ if __name__ == "__main__":
         [0, 6, 0, 0, 0, 0, 2, 8, 0],
         [0, 0, 0, 4, 1, 9, 0, 0, 5],
         [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+    grid_mala = [
+        [0,3,4,0],
+        [4,0,0,2],
+        [1,0,0,3],
+        [0,2,1,0]]
+    
 
     for solution in solve_sudoku((3, 3), grid):
         print(*solution, sep='\n')
     
 
-    
-    
+   
